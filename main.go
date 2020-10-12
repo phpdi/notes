7a57729cfcdd31e0bfab257895cb769a6225d841
goeoeo8122@gmail.com <977564830@qq.com>
2020-10-12 17:54:07 +0800

    中间件

--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(aaa())
+	test()
 
 }
 
 func aaa() int {
 	var i int
-	i=5;
+	i = 5
 	defer func() {
 		i++
 	}()
 
 	return i
+}
+
+func test() {
+	a := make([]int, 0, 5)
+	b := append(a, 1)
+	fmt.Println(a)
+	fmt.Println(b)
+
+	b[0] = 5
+	fmt.Println(a)
+	fmt.Println(b)
+
 }
