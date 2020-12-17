2f4ab24914836d4b94cbab677bab674d0a0a5546
goeoeo8122@gmail.com <977564830@qq.com>
2020-12-17 11:42:01 +0800

    简历

--- main.go
+++ main.go
@@ -1,32 +1,10 @@
 package main
-
-import "fmt"
-
-type User struct {
-	Id   int
-	Name []string
-}
-
-func (this *User) Run() {
-	this.Id = 4
-	this.Name[1] = "cc"
-}
-
-func (this User) Do() {
-	this.Id = 4
-	this.Name[1] = "cc"
-}
+import (
+	"fmt"
+)
 func main() {
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	d1 := slice[5:6:8]
+	fmt.Println(d1, len(d1), cap(d1))
 
-	//a:=User{
-	//	Id:   1,
-	//	Name: []string{"aa","bb"},
-	//}
-
-	b := &User{Id: 1,
-		Name: []string{"aa", "bb"},
-	}
-
-	b.Do()
-	fmt.Println(b)
 }
---
+++ source/_posts/go/go学习书籍.md
@@ -0,0 +1,11 @@
+
+#书栈网
+https://www.bookstack.cn/
+
+# 已读
+* Go入门指南(https://www.bookstack.cn/books/the-way-to-go_ZH_CN)
+* GO专家编程(https://www.bookstack.cn/books/GoExpertProgramming)
+
+# 阅读中
+
+* Go语言中文文档(https://www.bookstack.cn/books/topgoer)
