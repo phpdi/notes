00ebd048ab4000a004c80471fde16892b5b1a2f8
goeoeo8122@gmail.com <977564830@qq.com>
2020-12-18 18:30:53 +0800

    数据结构与算法

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
-import (
-	"fmt"
-)
-func main() {
-	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	d1 := slice[5:6:8]
-	fmt.Println(d1, len(d1), cap(d1))
 
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		b := i
+		defer func() {
+			fmt.Println(b)
+		}()
+	}
+}
+
+//输出 4,3,2,1,0
+
+func Rescuvie(n int, a int) int {
+	if n == 1 {
+		return a
+	}
+
+	return Rescuvie(n-1, n*a)
 }
