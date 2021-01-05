f4deac84c9e540d47d67105cb575b8013d655594
goeoeo8122@gmail.com <977564830@qq.com>
2021-01-05 17:11:28 +0800

    myql面试题

--- main.go
+++ main.go
@@ -1,23 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"time"
+)
+
+
+
 
 func main() {
-
-	for i := 0; i < 5; i++ {
-		b := i
-		defer func() {
-			fmt.Println(b)
-		}()
-	}
+	httpClient()
 }
 
-//输出 4,3,2,1,0
+func HttpServer()  {
+	mux:=http.DefaultServeMux
 
-func Rescuvie(n int, a int) int {
-	if n == 1 {
-		return a
+	mux.HandleFunc("/a", func(writer http.ResponseWriter, request *http.Request) {
+		key:=request.URL.Query().Get("key")
+
+		writer.Write([]byte(key))
+	})
+
+	s:=http.Server{
+		Addr:"127.0.0.1:4000",
+		Handler:mux,
 	}
 
-	return Rescuvie(n-1, n*a)
+	s.ListenAndServe()
+
+
+}
+
+func httpClient()  {
+	client:=getHttpClient()
+
+	for i:=0;i<10;i++ {
+		go func(i int) {
+			url:=fmt.Sprintf("http://127.0.0.1:4000/a?key=%d",i)
+			req,_:=http.NewRequest("GET", url, nil)
+			if resp, err := client.Do(req); err == nil {
+				if bodyByte, err := ioutil.ReadAll(resp.Body); err == nil {
+					fmt.Printf("请求：%d,响应:%s\n",i,string(bodyByte))
+				}
+
+				resp.Body.Close()
+			}
+
+		}(i)
+	}
+
+	time.Sleep(100*time.Second)
+
+}
+
+
+func getHttpClient() *http.Client {
+	var (
+		cookieJar *cookiejar.Jar
+	)
+	tr := &http.Transport{
+		DisableKeepAlives: true,                                  //禁用长连接
+		TLSClientConfig:   &tls.Config{InsecureSkipVerify: true}, //跳过证书验证
+	}
+	//http cookie接口
+	cookieJar, _ = cookiejar.New(nil);
+
+	client := &http.Client{
+		Jar:       cookieJar,
+		Transport: tr,
+	}
+
+	return client
+
 }
